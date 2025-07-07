ec799ad4ee84c6750088b19185785e87008a1dd9
de-lvin <delvinshoko@gmail.com>
2025-07-07 17:49:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js'; 
+import dbconnect from './database/mongodb.js';
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the subscription tracker service!');
+}
+);
+
+app.listen(PORT, async () => {
+  await dbconnect();
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
+
+export default app; 
---
+++ database/mongodb.js
@@ -0,0 +1,24 @@
+
+import mongoose from "mongoose";
+import { DATABASE_URI, NODE_ENV } from "../config/env.js";
+import  process from 'process';
+
+if (NODE_ENV === "development") {
+  mongoose.set("debug", true);
+}
+if (!DATABASE_URI) {
+  throw new Error("DATABASE_URI is not defined");
+}
+
+const dbconnect = async () => {
+    try{
+        await mongoose.connect(DATABASE_URI);
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("Error connecting to the database",error); 
+        process.exit(1);
+        
+    }
+}
+
+export  default dbconnect
